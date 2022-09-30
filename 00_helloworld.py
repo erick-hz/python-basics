101ede55cb1b3010d539be84b8adabc229d602df
Erick Hernandez <yerickk8@gmail.com>
2022-09-30 17:51:19 -0500

    Python-Basics

---
+++ 00_helloworld.py
@@ -0,0 +1,27 @@
+### Hola Mundo ###
+
+# Our hello world in Python
+print("Hola Python")
+print('Hola Python')
+
+# This is a comment
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Este también es un
+comentario
+en varias líneas
+'''
+
+# Cómo consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+print(type(print("Mi cadena de texto"))) # Tipo 'NoneType'
